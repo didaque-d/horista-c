9f6fbf68376984b133c3d6980f02c458e5e33886
didaque-d <diego.dads52@gmail.com>
2026-06-24 00:19:15 -0300

    feat: add initial HTML structure, logo, and JavaScript functionality
    - Created index.html with a basic layout including month selection and day selection sections.
    - Added logo.svg for branding in the header.
    - Implemented logic.js to handle month selection and navigation between sections.

---
+++ js/logic.js
@@ -0,0 +1,51 @@
+listaMeses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+function gravarMes(){
+    const mes = document.getElementById("selecionar-mes").value;
+    switch(mes){
+        case "0": 
+            console.log(listaMeses[0]);
+            break;
+        case "1":
+            console.log(listaMeses[1]);
+            break;
+        case "2":
+            console.log(listaMeses[2]);
+            break;
+        case "3":
+            console.log(listaMeses[3]);
+            break;
+        case "4":
+            console.log(listaMeses[4]);
+            break;
+        case "5":
+            console.log(listaMeses[5]);
+            break;
+        case "6":
+            console.log(listaMeses[6]);
+            break;
+        case "7":
+            console.log(listaMeses[7]);
+            break;
+        case "8":
+            console.log(listaMeses[8]);
+            break;
+        case "9":
+            console.log(listaMeses[9]);
+            break;
+        case "10":
+            console.log(listaMeses[10]);
+            break;
+        case "11":
+            console.log(listaMeses[11]);
+            break;
+}
+}
+function etapa1(){
+    gravarMes();
+    document.getElementById("cadastro-dias").style.display = "block";
+    document.getElementById("cadastro-mes").style.display = "none";
+}
+function voltar1(){
+    document.getElementById("cadastro-dias").style.display = "none";
+    document.getElementById("cadastro-mes").style.display = "block";
+}
