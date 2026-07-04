58c9fd3698063d0532b136f80a2c0a0840346b1a
didaque-d <diego.dads52@gmail.com>
2026-07-03 23:28:42 -0300

    fix: corrigir erros de digitação nos estilos e IDs de horas

--- js/logic.js
+++ js/logic.js
@@ -4,39 +4,51 @@ function gravarMes(){
     switch(mes){
         case "0": 
             console.log(listaMeses[0]);
+            document.getElementById("mes-escolhido").innerHTML = listaMeses[0];
             break;
         case "1":
             console.log(listaMeses[1]);
+            document.getElementById("mes-escolhido").innerHTML = listaMeses[1];
             break;
         case "2":
             console.log(listaMeses[2]);
+            document.getElementById("mes-escolhido").innerHTML = listaMeses[2];
             break;
         case "3":
             console.log(listaMeses[3]);
+            document.getElementById("mes-escolhido").innerHTML = listaMeses[3];
             break;
         case "4":
             console.log(listaMeses[4]);
+            document.getElementById("mes-escolhido").innerHTML = listaMeses[4];
             break;
         case "5":
             console.log(listaMeses[5]);
+            document.getElementById("mes-escolhido").innerHTML = listaMeses[5];
             break;
         case "6":
             console.log(listaMeses[6]);
+            document.getElementById("mes-escolhido").innerHTML = listaMeses[6];
             break;
         case "7":
             console.log(listaMeses[7]);
+            document.getElementById("mes-escolhido").innerHTML = listaMeses[7];
             break;
         case "8":
             console.log(listaMeses[8]);
+            document.getElementById("mes-escolhido").innerHTML = listaMeses[8];
             break;
         case "9":
             console.log(listaMeses[9]);
+            document.getElementById("mes-escolhido").innerHTML = listaMeses[9];
             break;
         case "10":
             console.log(listaMeses[10]);
+            document.getElementById("mes-escolhido").innerHTML = listaMeses[10];
             break;
         case "11":
             console.log(listaMeses[11]);
+            document.getElementById("mes-escolhido").innerHTML = listaMeses[11];
             break;
 }
 }
@@ -62,21 +74,34 @@ function voltarInicio(){
     document.getElementById("cadastro-horas").style.display = "none";
 
 }
-let qtdHoras = 0;
-function incrementarHora(){
-    qtdHoras++;
-    document.getElementById("hora-trabalhada").value = qtdHoras;
-    console.log(qtdHoras);
+/*
+let qtdHorasSeg = 0;
+function incrementarHoraSeg(){
+    qtdHorasSeg++;
+    document.getElementById("hora-seg").value = qtdHorasSeg;
+    console.log(qtdHorasSeg);
 }
-function decrementarHora(){
-    if(qtdHoras > 0){
-        qtdHoras--;
-        document.getElementById("hora-trabalhada").value = qtdHoras;
-        console.log(qtdHoras);
+function decrementarHoraSeg(){
+    if(qtdHorasSeg > 0){
+        qtdHorasSeg--;
+        document.getElementById("hora-seg").value = qtdHorasSeg;
+        console.log(qtdHorasSeg);
     }
 }
 
-
+let qtdHorasTer = 0;
+function incrementarHoraTer(){
+    qtdHorasTer++;
+    document.getElementById("hora-ter").value = qtdHorasTer;
+    console.log(qtdHorasTer);
+}
+function decrementarHoraTer(){
+    if(qtdHorasTer > 0){
+        qtdHorasTer--;
+        document.getElementById("hora-ter").value = qtdHorasTer;
+        console.log(qtdHorasTer);
+    }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.dias-btn');
@@ -85,6 +110,7 @@ document.addEventListener('DOMContentLoaded', () => {
             if(event.target.tagName === 'BUTTON') {
                 event.target.classList.toggle('ativo');
                 document.getElementById("segunda").style.display = 'block';
+                console.log(event);
             }
         });
     } else{
@@ -92,3 +118,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 })
 
+*/
